ef398c5b7f78f999810eed577d5f8fb65468bd6a
naveenbasu-11 <naveenbasubasu@gmail.com>
2026-07-03 18:52:23 +0530

    my first commit

---
+++ eclipse-workspace/batch75/src/batch75/rectangle.java
@@ -0,0 +1,24 @@
+package batch75;
+
+public class rectangle {
+	void CalculaterArea() {
+		int length=10;
+		int breadth=5;
+		
+		int Area=length*breadth;
+		int perimeter=2*(length+breadth);
+		System.out.println("area of rectangle:"+ Area);
+		System.out.println("perimeter of rectangle:"+ perimeter);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		rectangle A = new rectangle();
+		
+		A.CalculaterArea();
+		
+		
+	}
+
+}
