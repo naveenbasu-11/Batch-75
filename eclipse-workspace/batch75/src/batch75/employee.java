9444cff8e2cc11599b19c02ff2d963df634ecc28
naveenbasu-11 <naveenbasubasu@gmail.com>
2026-07-22 19:03:01 +0530

    LOCAL VARIABLES

---
+++ eclipse-workspace/batch75/src/batch75/employee.java
@@ -0,0 +1,28 @@
+package batch75;
+
+public class employee {
+	int employeeId=1;
+	char EmpGrade='A';
+	int EmpSalary=30000;
+	boolean empStatus=true;
+	int WorkingDays=22;
+	long pHNumber=9390646359L;
+	int BonusAmount=20000;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		employee N = new employee();
+		
+		System.out.println("Employee Id:"+N.employeeId);
+		System.out.println("Employee Grade:"+N.EmpGrade);
+		System.out.println("Employee Grade:"+N.EmpSalary);
+		System.out.println("Employee status:"+N.empStatus);
+		System.out.println("Employee WorkingDays:"+N.pHNumber);
+		System.out.println("Employee Phone Number:"+N.pHNumber);
+		System.out.println("Employee Bonus Amount:"+N.BonusAmount);
+		
+		
+	}
+
+}
---
+++ eclipse-workspace/batch75/src/batch75/movieTicket.java
@@ -0,0 +1,21 @@
+package batch75;
+
+public class movieTicket {
+	public static void main(String[]args) {
+		int ticketId=2034;
+		int ticketPrice=275;
+		boolean BookingStatus=true;
+		int viewerAge=18;
+		int numberofTickets=700;
+		long contactNumber=9390646359L;
+		int gstAmount=16;
+	System.out.println("Ticket Id:"+ticketId);	
+	System.out.println("Ticket Price:"+ticketPrice);	
+	System.out.println("Ticket status:"+BookingStatus);	
+	System.out.println("Ticket viewer Age:"+viewerAge);	
+	System.out.println("Ticket Number of Tickets:"+numberofTickets);	
+	System.out.println("Ticket Contact Number:"+contactNumber);	
+	System.out.println("Ticket GST Amount:"+gstAmount);	
+	}
+
+}
